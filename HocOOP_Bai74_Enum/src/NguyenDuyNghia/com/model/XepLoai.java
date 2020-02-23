ae0bec617ecd56e2038d558ad26699cf2a1be99b
ASUS <ASUS@192.168.1.3>
2020-02-23 16:24:27 +0700

    HocOOP_Bai74_Enum

---
+++ HocOOP_Bai74_Enum/src/NguyenDuyNghia/com/model/XepLoai.java
@@ -0,0 +1,18 @@
+package NguyenDuyNghia.com.model;
+
+public enum XepLoai {
+	Gioi ("Giỏi"),
+	Kha ("Khá"),
+	TrungBinh("Trung Bình"),
+	Yeu("Yếu");
+	private String msg;
+	XepLoai(String msg)
+	{
+		this.msg=msg;
+	}
+	public String description()
+	{
+		return this.msg;
+	}
+
+}
